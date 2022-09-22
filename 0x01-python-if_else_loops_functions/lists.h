d40a5fa3a33294b72a086520bce69a9e79a014ea
Nenkkyoung <ynenzy@gmail.com>
2022-09-21 17:39:14 -0700

    lists.h

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,5 +1,6 @@
+#ifndef LISTS_H
 #define LISTS_H
-#include <stdio.h>
+
 #include <stdlib.h>
 
 /**
@@ -8,6 +9,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ * for Holberton project
  */
 typedef struct listint_s
 {
